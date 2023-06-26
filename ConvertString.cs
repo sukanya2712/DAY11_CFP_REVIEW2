25817cd488853bc5e16425a6a704613f5f61434f
sukanya2712 <sukanyanaik511@gmail.com>
2023-06-27 00:23:18 +0530

    Added

---
+++ ConvertString.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace review2
+{
+    internal class ConvertString
+    {
+        string word = "Happy BirthDay";
+        string answord = string.Empty;
+        //char.IsUpper('a')-> false
+
+        /*for (int i=0; i<word.Length; i++)
+        {
+            if (char.IsUpper(word[i]))
+            {
+                answord += char.ToLower(word[i]);
+            }
+            else 
+            {
+                answord += char.ToUpper(word[i]);
+            }
+
+        }
+
+        Console.WriteLine(answord);*/
+
+        public static string Method2(string word)
+        {
+            
+            string answord = string.Empty;
+
+            
+
+            
+
+                for (int i=0; i<word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        answord += char.ToLower(word[i]);
+                    }
+                    else 
+                    {
+                        answord += char.ToUpper(word[i]);
+                    }
+
+                }
+
+            return answord;
+
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace review2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*input: "Happy BirthDay"
+    output: "hAAPY bIRTHdAY"*/
+
+            //string word = "Happy BirthDay";
+            // string answord = string.Empty;
+            //char.IsUpper('a')-> false
+
+            /*for (int i=0; i<word.Length; i++)
+            {
+                if (char.IsUpper(word[i]))
+                {
+                    answord += char.ToLower(word[i]);
+                }
+                else 
+                {
+                    answord += char.ToUpper(word[i]);
+                }
+               
+            }
+
+            Console.WriteLine(answord);*/
+
+            Console.WriteLine(ConvertString.Method2("Happy BirthDay"));
+
+
+        }
+    }
+}
